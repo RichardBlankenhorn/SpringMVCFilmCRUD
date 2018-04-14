b6bfd8ce2219d9e7fb2a44ce64f03fcf957597bc
alanfrederick <alan@alanfrederick.com>
2018-04-14 09:57:16 -0600

    add methods to film controller

--- src/main/java/com/skilldistillery/film/dao/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDAO.java
@@ -6,11 +6,17 @@ import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
 
 public interface FilmDAO {
-	public Film getFilmById(String filmId);
-	public void addFilm(Film film);
+	public Film getFilmById(String filmId); // added to controller
+
+	public void addFilm(Film film); // added to controller
+
 	public Film deleteFilm(Film film);
+
 	public Film updateFilm(Film film);
-	public List<Film> getFilmBySearchKeyword(String keyword);
-	public List<Actor> getActorsByFilmId(String filmId);
-	public Actor getActorById(int actorID);
+
+	public List<Film> getFilmBySearchKeyword(String keyword); // added to controller
+
+	public List<Actor> getActorsByFilmId(String filmId); // added to controller
+
+	public Actor getActorById(int actorID); //added to controller
 }
